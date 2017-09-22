45e2fa54b2cf2c0455ea3bd4250e06f74b1ef658
S73FF3N <stschroedter@yahoo.de>
2017-09-22 21:39:24 +0200

    Added multiple comments

--- Weristdas.py
+++ Weristdas.py
@@ -27,19 +27,18 @@ def wid(players, playerNamesList, im_dir):
 	playerNames = playerNamesList
 	playerScore = [0]*players
 	# key definitions
-	playerKeys = [0, 5, 10, 15]
-	answer = [K_r, K_f] # 12: correct, 13: wrong
+	playerKeys = [0, 5, 10, 15] # these definitions are valid for PS2 Buzz! Controllers
+	answer = [K_r, K_f] # K_r: correct, K_f: incorrect
 	
 	# Define the colours used in RGB format
 	black = [ 0, 0, 0]
 	white = [255,255,255]
 	red = [255, 0, 0]
 	
-	#numberPlayers = len(playerScore)
 	print "number of players:", players
 	
 	# set the screen the highest resolution for this display
-	screen=pygame.display.set_mode((1200,800), pygame.FULLSCREEN)#(1800, 860))#
+	screen=pygame.display.set_mode((1200,800), pygame.FULLSCREEN)
 	# Get the width and height of the screen as we don't know it for fullscreen
 	screenx, screeny = screen.get_size()
 	# starting with a gap of 20 on the left and 20 on the right figure out the location for the squares.
@@ -51,7 +50,7 @@ def wid(players, playerNamesList, im_dir):
 	picture_length = 600
 	
 	# text displayed at the beginning
-	welcome = "Willkommen bei 'Wer ist das?'"
+	welcome = "Welcome to 'Wer ist das?'"
 	
 	# build image dictionary from image directory
 	# images in image directory are converted into .bmp    
@@ -66,6 +65,7 @@ def wid(players, playerNamesList, im_dir):
 	
 	image_dict = {}
 	
+	# function to build image dictionary with solution as key and image as value
 	def build_image_dict(image):
 		base=os.path.basename(im_path+image)
 		name_o=os.path.splitext(base)[0]
@@ -73,17 +73,19 @@ def wid(players, playerNamesList, im_dir):
 		image_dict[name] = im_path+image
 
 	for file_in in im_dir_content_list:
+		# to avoid loading mac specific files that may be created
 		if file_in == ".DS_Store":
 	    		continue
 		elif file_in.startswith("._") == True:
 			continue
+		# convert images to pygame friendly image format .bmp
 		else:
 	    		converted_file = convert_image_to(file_in, "bmp")
 	    		build_image_dict(converted_file) 
                    
-	print image_dict
 	print "number of pictures:", len(image_dict)
-	# randomly chosing image from image dictionary and updating image label            
+	# randomly chosing image from image dictionary and updating image label
+	# image that has been played is deleted from dictionary
 	def random_pick_image():
 		global random_im_key
 		global random_im_val
@@ -93,10 +95,11 @@ def wid(players, playerNamesList, im_dir):
 	    		del image_dict[random_im_key]
 		except:
 	    		random_im_val = os.path.join('images', logo)
-	    		random_im_key = "Alle Bilder gespielt! Ende"
+	    		random_im_key = "All images played! Ending"
 		random_image = load_image(random_im_val, im_dir)
 		image_size = random_image.get_rect().size
 		rela = image_size[0]/float(image_size[1])
+		# adjusting image width/height to screen
 		# image wider than high
 		if rela >= 1:
 		    image_size = (800, int(min(600, 800/rela)))
@@ -108,7 +111,7 @@ def wid(players, playerNamesList, im_dir):
 		pygame.draw.rect(screen, white, ((screenx/2)-(picture_width/2), 5,picture_width, picture_length), 0)
 		screen.blit(random_image, ((screenx/2)-(image_size[0]/2), (605-image_size[1])))
 		if not bool(image_dict) and random_im_val == logo:
-		    solution_label = myfont.render("Alle Bilder gespielt! Ende!", 1, red)
+		    solution_label = myfont.render("All images played! Ending!", 1, red)
 		    screen.blit(solution_label, (screenx/2 - len(random_im_key)*10/2, picture_length+squaresize+70))
 		if random_im_key == '.DS Store':
 		    random_pick_image()
@@ -164,15 +167,17 @@ def wid(players, playerNamesList, im_dir):
 	
 	first = 0 # used to signify the first key pressed and stops other being used
 	waitReset = 1 # Reset section for the while loop
-	show_solution_var = 1
-	initialize = 1
+	show_solution_var = 1 # variable to indicate if game is in the solution phase
+	initialize = 1 # variable to indicate if game has been initialized by referee
 	
 	while True:
 		while initialize == 1:
 			for event in pygame.event.get():   
 			        if event.type == pygame.KEYDOWN:
+					# terminate game with escape
 				 	if event.key == K_ESCAPE:
 						pygame.quit()
+					# referee uses return key to control the game
 					if event.key == K_RETURN:
 						pygame.draw.rect(screen, white, (0, picture_length+squaresize+65, 1500,50),0)
 		                    		pygame.display.flip()                            
@@ -182,7 +187,7 @@ def wid(players, playerNamesList, im_dir):
 					else:
                                                 pass
 						
-		# Stay in the loop until one of the 'button' keys is pressed
+		# Stay in the loop until one of the 'buzzer' keys is pressed or referee shows next image manually
 		while first == 0:
 		    for event in pygame.event.get():   
 		        if event.type == pygame.KEYDOWN:
@@ -194,9 +199,8 @@ def wid(players, playerNamesList, im_dir):
 						show_solution_var = 1
 					except:
 						show_solution_var = 2
-		                
-		        if event.type == pygame.JOYBUTTONDOWN:
-		            # finding a way to check for any number of keys
+		        # one buzzer is pressed        
+		        if event.type == pygame.JOYBUTTONDOWN
 		            buttonpressed = event.button
 		                
 		            for n in range (0,players):
@@ -207,20 +211,23 @@ def wid(players, playerNamesList, im_dir):
 		                    if players % 2 != 0:
 		                        xpos = (screenx/2)-(((players/2)-first_buzz)*squarewidth)-(squarewidth/2)
 		
+				    # indicates which player buzzered first with an red square
 		                    pygame.draw.rect(screen, red, (xpos, picture_length+30,squaresize,squaresize), 0)
 		                    first = 1
+				    # countdown starts to leave player 5 seconds to answer
 		                    countdown(5)
 		                
 		            pygame.display.flip()
-		            # a 'button' was pressed and shown on screen
+		            # a 'buzzer' was pressed and shown on screen
 		            # now go to the reset code
 		
-		# loop waiting until the 'button' are reset
+		# loop waiting until the 'buzzers' are reset
 		waitReset=0
 		# put an x on the screen to show it is in lockout mode
 		screen.blit(scorefont.render("X",1,black),(screenx-40, 10))
 		pygame.display.flip()
 		
+		# reset mode (decision if answer was correct and points are given by referee, red square is reset to black)
 		while waitReset == 0:
 		        for event in pygame.event.get():	
 		            if event.type == pygame.KEYDOWN and event.key == K_ESCAPE:
@@ -230,7 +237,7 @@ def wid(players, playerNamesList, im_dir):
 			    if event.type == pygame.KEYDOWN:
 		                keypressed = event.key
 		                                       
-		                # Check if Key Pressed to increase score
+		                # points are given for player who buzzered
 		                if keypressed in answer:
 		                    # blank out score label
 		                    pygame.draw.rect(screen, white, (xpos, picture_length+squaresize+30, squaresize, 30),0)
